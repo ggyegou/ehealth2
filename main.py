fd2a5793fe53c1cad7ff8251af5a5c105cf29160
ggyegou <294497650@qq.com>
2017-04-27 23:36:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,27 @@ import os.path
 import os
 from  PreProcess import *
 import w2v
+#the directory of queries
 path = "D:\\eHealth\\data of eHealth Task 2\\topics\\"
+#the directory of crawled data
 corpus_path = "D:\\eHealth\\data of eHealth Task 2\\processed\\"
+#the result file
 output_file = open("D:\\eHealth\\data of eHealth Task 2\\sorted\\result.txt","w+",encoding="utf-8")
+#load word vectors
 w2v.init()
 from ListExe import *
-for parent,dirnames,filenames in os.walk(path):    #三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
 
+for parent,dirnames,filenames in os.walk(path):   
+#for each topic file
     for filename in filenames:
         file = path + filename
         print (file)
+        #extract topicid,title,query,pids from topic file
         (topic,title,query,pids) = read(file)
+        #pre-process the query
         query = query_process(query.lower(),"stoplist.txt")
         query_tokens = query.split(" ")
+        #generate a vector of 300d like [1,1,1,1,1....,1,1] to avoid zero division
         topic_vec = generateI()
         size1 = 1
         for token in query_tokens:
@@ -31,6 +39,7 @@ for parent,dirnames,filenames in os.walk(path):    #三个参数：分别返回1
         #=====================get topic vec finish===============
         scores = dict()
         for pid in pids:
+            #for each pid listed in topic file
             content = ""
             abs_vec = generateI()
             size2 = 1
@@ -55,6 +64,7 @@ for parent,dirnames,filenames in os.walk(path):    #三个参数：分别返回1
                 abs_vec = generateI()
 
             #=============get pid's vec finished===============
+            #compute similarity between query and abstract
             score = cosvalue(topic_vec,abs_vec)
             scores[pid] = score
         res = sorted(scores.items(), key=lambda item: item[1], reverse=True)
