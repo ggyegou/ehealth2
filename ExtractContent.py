0efff330b92f21e823278b01b76b06c5bcc2304f
ggyegou <294497650@qq.com>
2017-04-27 23:37:28 +0800

    Update ExtractContent.py

--- ExtractContent.py
+++ ExtractContent.py
@@ -23,26 +23,3 @@ def read(path):
         if "Pids" in line:
             state = 3
     return (topic,title,query,pids)
-'''
-path = "D:\\eHealth\\data of eHealth Task 2\\topics\\"
-output = "D:\\eHealth\\data of eHealth Task 2\\topic_processed\\"
-for parent,dirnames,filenames in os.walk(path):    #三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
-
-    for filename in filenames:
-        file = path + filename
-        print (file)
-        (topic,title,query,pids) = read(file)
-        query = query_process(query.lower(),"stoplist.txt")
-        content = "<top>"
-        content += "<topic>"+topic+"</topic>\n"
-        content += "<title>"+title+"</title>\n"
-        content += "<query>"+query+"</query>\n"
-        content += "<pids>\n"
-        for pid in pids:
-            content+=pid+"\n"
-        content += "</pids>\n"
-        content+="</top>"
-        output_file = open(output+filename,"w+",encoding="utf-8")
-        output_file.write(content)
-        output_file.close()
-'''
